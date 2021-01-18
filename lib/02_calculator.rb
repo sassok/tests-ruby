e441f9b97c41f6805be48e70ac8f0c2223a4a35c
sassok <kevin.sassoo@gmail.com>
2021-01-18 13:11:27 +0100

    finish

---
+++ lib/02_calculator.rb
@@ -0,0 +1,48 @@
+def add(num, num1)
+	value = num + num1
+	return value
+end
+
+
+def subtract(num, num1)
+	value = num - num1
+	return value
+end
+
+
+def sum(num)
+	value = 0
+	num.each {|num| value += num}
+	return value
+end
+
+
+def multiply(num1, num2)
+	value = num1 * num2
+	return value
+end
+
+def power(num, num1)
+	value = num ** num1
+	return value
+end
+
+def factorial(num)
+	if num == 0
+	return value = 1
+
+	else 
+		value = 1
+		num.times do |x|
+		value = value * (x + 1)
+
+		
+
+end
+return value
+end
+
+end
+
+
+	
